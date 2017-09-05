bc02cf9a4ea649b742278da2f364dfb42c799df3
eric thul <thul.eric@gmail.com>
2017-09-04 20:15:54 -0400

    Update

--- src/Example/App.purs
+++ src/Example/App.purs
@@ -1,6 +1,6 @@
 module Example.App (main) where
 
-import Prelude (Unit, (<$>), (+), (<>), (<<<), bind, id, pure, show, void)
+import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, info)
@@ -8,7 +8,8 @@ import Control.Monad.Eff.Timer (TIMER, setTimeout)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
 
 import Data.Lens (Lens', Prism', lens, prism')
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
+import Data.Newtype (wrap)
 
 import React as React
 import React.DOM as DOM
@@ -65,62 +66,115 @@ store = Redux.createStore reducer initialState (middlewareEnhancer <<< reduxDevt
          ActionA (DelayedIncrementA delay) -> void (setTimeout delay (void (next action)))
          _ -> void (next action)
 
-  reducer :: Redux.Reducer Action State
-  reducer action =
-    (Redux.reducerOptic lensA prismA reducerA) action <<<
-    (Redux.reducerOptic lensB prismB reducerB) action
+  reducer :: Redux.Reducer' Action State
+  reducer =
+    Redux.reducerOptic lensA prismA reducerA <<<
+    Redux.reducerOptic lensB prismB reducerB
     where
-    reducerA :: Redux.Reducer ActionA StateA
-    reducerA action' state' =
-      case action' of
-           IncrementA -> state' { counterA = state'.counterA + 1 }
-           DelayedIncrementA _ -> state' { counterA = state'.counterA + 1 }
+    reducerA :: Redux.Reducer' ActionA StateA
+    reducerA = wrap $ \action state ->
+      case action of
+           IncrementA -> state { counterA = state.counterA + 1 }
+           DelayedIncrementA _ -> state { counterA = state.counterA + 1 }
 
     lensA :: Lens' State StateA
     lensA = lens (\s -> { counterA: s.counterA }) (\s b -> s { counterA = b.counterA })
 
     prismA :: Prism' Action ActionA
-    prismA = prism' ActionA (\a -> case a of
-                                        ActionA a' -> Just a'
-                                        _ -> Nothing)
+    prismA = prism' ActionA $
+      case _ of
+           ActionA a -> Just a
+           _ -> Nothing
 
-    reducerB :: Redux.Reducer ActionB StateB
-    reducerB action' state' =
-      case action' of
-           IncrementB -> state' { counterB = state'.counterB + 1 }
+    reducerB :: Redux.Reducer' ActionB StateB
+    reducerB = wrap $ \action state ->
+      case action of
+           IncrementB -> state { counterB = state.counterB + 1 }
 
     lensB :: Lens' State StateB
     lensB = lens (\s -> { counterB: s.counterB }) (\s b -> s { counterB = b.counterB })
 
     prismB :: Prism' Action ActionB
-    prismB = prism' ActionB (\a -> case a of
-                                        ActionB a' -> Just a'
-                                        _ -> Nothing)
+    prismB = prism' ActionB $
+      case _ of
+           ActionB a -> Just a
+           _ -> Nothing
 
-appClass :: Redux.ReduxReactClass' State State
-appClass = Redux.createClass' id (Redux.spec' render)
+type IncrementAProps eff
+  = { a :: Int
+    , onIncrement :: Maybe Int -> Eff eff Unit
+    }
+
+incrementAClass :: forall eff. React.ReactClass (IncrementAProps eff)
+incrementAClass = React.createClassStateless render
   where
-  render :: forall eff. Redux.Render State Unit eff (Eff (Redux.ReduxEffect eff)) Action
-  render dispatch this = render' <$> React.getProps this
+  render :: IncrementAProps eff -> React.ReactElement
+  render { a
+         , onIncrement
+         } =
+    DOM.div []
+            [ DOM.button [ Props.onClick (const $ unsafeCoerceEff (onIncrement Nothing)) ]
+                         [ DOM.text ("Increment A: " <> show a) ]
+            , DOM.button [ Props.onClick (const $ unsafeCoerceEff (onIncrement (Just 2000))) ]
+                         [ DOM.text ("Increment A (delayed by 2s): " <> show a) ]
+            ]
+
+type IncrementBProps eff
+  = { b :: Int
+    , onIncrement :: Eff eff Unit
+    }
+
+incrementBClass :: forall eff. React.ReactClass (IncrementBProps eff)
+incrementBClass = React.createClassStateless render
+  where
+  render :: IncrementBProps eff -> React.ReactElement
+  render { b
+         , onIncrement
+         } =
+    DOM.div []
+            [ DOM.button [ Props.onClick (const $ unsafeCoerceEff onIncrement) ]
+                         [ DOM.text ("Increment B: " <> show b) ]
+            ]
+
+incrementAComponent :: forall eff. Redux.ConnectClass' State (IncrementAProps eff)
+incrementAComponent = Redux.connect stateToProps dispatchToProps incrementAClass
+  where
+  stateToProps :: State -> { } -> { a :: Int }
+  stateToProps { counterA } ownProps = { a: counterA }
+
+  dispatchToProps :: Redux.Dispatch' eff Action -> { } -> { onIncrement :: Maybe Int -> Eff eff Unit }
+  dispatchToProps dispatch ownProps = { onIncrement: void <<< unsafeCoerceEff <<< dispatch <<< ActionA <<< maybe IncrementA DelayedIncrementA }
+
+incrementBComponent :: forall eff. Redux.ConnectClass' State (IncrementBProps eff)
+incrementBComponent = Redux.connect stateToProps dispatchToProps incrementBClass
+  where
+  stateToProps :: State -> { } -> { b :: Int }
+  stateToProps { counterB } _ = { b: counterB }
+
+  dispatchToProps :: Redux.Dispatch' eff Action -> { } -> { onIncrement :: Eff eff Unit }
+  dispatchToProps dispatch _ = { onIncrement: void (unsafeCoerceEff (dispatch (ActionB IncrementB))) }
+
+type AppProps = Unit
+
+appClass :: React.ReactClass AppProps
+appClass = React.createClass (React.spec unit render)
+  where
+  render :: forall eff. React.Render AppProps Unit eff
+  render this = render' <$> React.getProps this
     where
-    render' :: State -> React.ReactElement
-    render' props =
+    render' :: AppProps -> React.ReactElement
+    render' _ =
       DOM.div []
-              [ DOM.button [ Props.onClick (onClick (ActionA IncrementA)) ]
-                           [ DOM.text ("Increment A: " <> show props.counterA) ]
-              , DOM.button [ Props.onClick (onClick (ActionA (DelayedIncrementA 2000))) ]
-                           [ DOM.text ("Increment A (delayed by 2s): " <> show props.counterA) ]
-              , DOM.button [ Props.onClick (onClick (ActionB IncrementB)) ]
-                           [ DOM.text ("Increment B: " <> show props.counterB) ]
+              [ Redux.createElement_ incrementAComponent []
+              , Redux.createElement_ incrementBComponent []
               ]
-      where
-      onClick :: Action -> React.Event -> React.EventHandlerContext eff Unit Unit Unit
-      onClick action event = void (unsafeCoerceEff (dispatch (pure action)))
 
 main :: forall eff. Eff (Effect (Redux.ReduxEffect eff)) React.ReactElement
 main = do
   store' <- store
-  let element = Redux.createProviderElement store' appClass
+
+  let element = Redux.createProviderElement store' [ React.createElement appClass unit [] ]
+
   pure element
 
 foreign import reduxDevtoolsExtensionEnhancer :: forall action state. Redux.EnhancerForeign action state
